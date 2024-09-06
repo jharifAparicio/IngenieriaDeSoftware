6b79170ee9097f6ab9fa5448331e5e30fccd3391
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-06 14:06:23 -0400

    Refactorizar modelo de usuario y agregar funcionalidades

--- laboratorio 4/user_repository.py
+++ laboratorio 4/user_repository.py
@@ -9,10 +9,15 @@ class UserRepository:
     @staticmethod
     def get_user_by_id(user_id):
         return User.query.get(user_id)
+    
+    @staticmethod
+    def get_user_by_username(username):
+        return User.query.filter_by(username=username).first()
 
     @staticmethod
-    def create_user(username, password):
-        new_user = User(username=username, password=password)
+    def create_user(name,username, email,password):
+        new_user = User(name=name, username=username, email=email)
+        new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
         return new_user
