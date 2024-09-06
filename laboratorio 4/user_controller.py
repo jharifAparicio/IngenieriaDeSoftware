20b72cbe2dece360ee91da22c17de815a0a3ed2f
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-06 14:06:32 -0400

    Refactorizar modelo de usuario y agregar funcionalidades

--- laboratorio 4/user_controller.py
+++ laboratorio 4/user_controller.py
@@ -1,17 +1,18 @@
 from flask_restful import Api, Resource
+from flask import request, jsonify
+from werkzeug.security import check_password_hash
 from user_repository import UserRepository
 
 def create_api(app):
     api = Api(app)
-
     class UserResource(Resource):
         def get(self):
             users = UserRepository.get_all_users()
             return [{"id": user.id, "username": user.username, "password":user.password} for user in users]
 
         def post(self):
-            data = app.request.get_json()
-            new_user = UserRepository.create_user(data['username'], data['password'])
+            data = request.get_json()
+            new_user = UserRepository.create_user(data['name'], data['username'], data['email'], data['password'])
             return {"id": new_user.id, "username": new_user.username}
 
     class SingleUserResource(Resource):
@@ -33,8 +34,21 @@ def create_api(app):
             if user is None:
                 return {"message": "User not found"}, 404
             return {"message": "User deleted successfully"}
-
+        
+    class LoginResource(Resource):
+        def post(self):
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+            
+            # Buscar usuario por username
+            user = UserRepository.get_user_by_username(username)
+            if user and check_password_hash(user.password, password):
+                return {"message": "Login successful", "user": {"id": user.id, "username": user.username}}
+            return {"message": "Invalid credentials"}, 401
+        
     api.add_resource(UserResource, '/users')
     api.add_resource(SingleUserResource, '/users/<int:user_id>')
+    api.add_resource(LoginResource, '/login')
 
     return api
